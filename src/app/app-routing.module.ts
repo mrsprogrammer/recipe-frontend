9d9a250feaed58688eb34a70ec10a2af47e16f20
justyna <you@example.com>
2020-10-06 20:38:25 +0200

    #main: travel component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
+import { Travel } from "./travel/travel.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "travel",
+    component: Travel,
+    data: { title: "Travel" },
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/travel/travel.component.ts
@@ -0,0 +1,11 @@
+import { Component, NgModule, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-travel",
+  templateUrl: "./travel.component.html",
+})
+export class Travel implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
