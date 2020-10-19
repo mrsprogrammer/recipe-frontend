4a72850f9ad47eb86fc2c4dd1085ada7832057d5
justyna <you@example.com>
2020-10-19 19:53:30 +0200

    #main: update

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,19 @@
 import { Component, NgModule, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: "app-main",
   templateUrl: "./main.component.html",
 })
 export class Main implements OnInit {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // Simple GET request with response type <any>
+    this.http
+      .get<any>("http://localhost:8080/api/members")
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
 }
