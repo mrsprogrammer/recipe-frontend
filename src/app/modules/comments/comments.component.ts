7c1ef48419ffe798c8d06347479ea7b0f929dfb5
justyna <you@example.com>
2020-12-07 22:22:35 +0100

    #main: update

--- src/app/modules/comments/comments.component.ts
+++ src/app/modules/comments/comments.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { Comment as CommentModel } from "../../model/comment";
 
 @Component({
